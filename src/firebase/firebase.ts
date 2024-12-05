eed2ddfb3405a1d2ca957c99ba198d9253b83677
Julius Oyovwikigho <julius.dev1991@gmail.com>
2024-12-05 11:32:45 +0000

    fix pnpm lint error

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { env } from "process";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
